e39b4d4accafaa37055fc1a5305b8daddd8f6622
Lukas Waymann <lb.waym@gmail.com>
2013-12-29 14:01:39 +0100

    Don't close() files when opening failed

--- notion/statusd_charge.lua
+++ notion/statusd_charge.lua
@@ -39,6 +39,9 @@ end
 local files = {charge_now, status,}
 
 local function update_charge()
+    files.charge_now = nil
+    files.status = nil
+
     files.charge_now = io.open(settings.path .. settings.filenames.charge_now,
         "r")
     files.status = io.open(settings.path .. settings.filenames.status, "r")
@@ -47,11 +50,11 @@ local function update_charge()
         charge_now = tonumber(files.charge_now:read("*all")) / 1000
         if files.status then
             status = files.status:read("*all")
+            files.status:close()
         end
+        files.charge_now:close()
     end
 
-    files.charge_now:close()
-    files.status:close()
     inform_charge(charge_now, status)
     charge_timer:set(settings.update_interval, update_charge)
 end
