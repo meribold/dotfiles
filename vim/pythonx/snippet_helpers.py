b6f83bfc1a5731c2c287b7992d78df1a002494a2
Lukas Waymann <lb.waym@gmail.com>
2015-11-30 17:43:21 +0100

    Allow passing overrides to modeline snippet helper
    The modeline function accepts a dictionary argument with modeline
    options that will override the defaults now.

--- vim/pythonx/snippet_helpers.py
+++ vim/pythonx/snippet_helpers.py
@@ -5,10 +5,23 @@ from collections import OrderedDict
 
 import vim
 
-modeline_options = OrderedDict([("tw", "=90"), ("sts", "=-1"), ("sw", "=3"), ("et", "")])
+# All options that might be used in modelines should be listed here.
+modeline_options = OrderedDict([("tw", "=90"), ("ts", None), ("sts", "=-1"), ("sw", "=3"),
+                                ("et", True)])
 
-def modeline():
-    options = " ".join("{!s}{!s}".format(k, v) for (k, v) in modeline_options.items())
+def modeline(overrides = {}):
+    options = []
+    for k, v in modeline_options.items():
+        setting = overrides[k] if k in overrides else  v
+        if setting is None:
+            continue
+        elif not setting:
+            options.append("no" + k)
+        elif isinstance(setting, str):
+            options.append("{!s}{!s}".format(k, setting))
+        else:
+            options.append(k)
+    options = " ".join(options)
     commentstring = vim.eval("&commentstring")
     if vim.eval("&ft") and commentstring:
         return re.sub(" ?%s ?", " vim: %s " % options, commentstring).strip()
