7dca9069bd65336b5a9a9fbca2750329ae003b43
Lukas Waymann <lb.waym@gmail.com>
2015-11-30 17:02:05 +0100

    Move modeline snippet helper function
    The function is in snippet_helpers.py now and is imported from
    all.snippets.

--- vim/pythonx/snippet_helpers.py
+++ vim/pythonx/snippet_helpers.py
@@ -1,5 +1,18 @@
 # Stuff used in UltiSnips snippets.
 
+import re
+from collections import OrderedDict
+
 import vim
+
+modeline_options = OrderedDict([("tw", "=90"), ("sts", "=-1"), ("sw", "=3"), ("et", "")])
+
+def modeline():
+   options = " ".join("{!s}{!s}".format(k, v) for (k, v) in modeline_options.items())
+   commentstring = vim.eval("&commentstring")
+   if vim.eval("&ft") and commentstring:
+      return re.sub(" ?%s ?", " vim: %s " % options, commentstring).strip()
+   else:
+      return "vim: " + options
 
 # vim: tw=90 sts=-1 sw=4 et
