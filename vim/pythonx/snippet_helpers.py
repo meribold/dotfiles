c7aabb766d2a92d085f302989b0f749d1dcdbac0
Lukas Waymann <lb.waym@gmail.com>
2015-11-30 17:10:45 +0100

    Fix indentation in snippet_helpers.py
    Using 4 spaces per indentation level is recommended for Python scripts
    in [PEP 8](https://www.python.org/dev/peps/pep-0008/).

--- vim/pythonx/snippet_helpers.py
+++ vim/pythonx/snippet_helpers.py
@@ -8,11 +8,11 @@ import vim
 modeline_options = OrderedDict([("tw", "=90"), ("sts", "=-1"), ("sw", "=3"), ("et", "")])
 
 def modeline():
-   options = " ".join("{!s}{!s}".format(k, v) for (k, v) in modeline_options.items())
-   commentstring = vim.eval("&commentstring")
-   if vim.eval("&ft") and commentstring:
-      return re.sub(" ?%s ?", " vim: %s " % options, commentstring).strip()
-   else:
-      return "vim: " + options
+    options = " ".join("{!s}{!s}".format(k, v) for (k, v) in modeline_options.items())
+    commentstring = vim.eval("&commentstring")
+    if vim.eval("&ft") and commentstring:
+        return re.sub(" ?%s ?", " vim: %s " % options, commentstring).strip()
+    else:
+        return "vim: " + options
 
 # vim: tw=90 sts=-1 sw=4 et
