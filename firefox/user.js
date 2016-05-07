6420e2cfce824ece4b854d3b9ce669e467bc11c0
Lukas Waymann <meribold@gmail.com>
2016-05-07 20:10:14 +0200

    Disable the Pocket Firefox extension

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,10 @@
 // Don't animate showing and hiding of the tab bar etc. when in fullscreen mode.
 user_pref('browser.fullscreen.animate', false);
 
+// Disable [Pocket](https://en.wikipedia.org/wiki/Pocket_%28application%29).
+user_pref('extensions.pocket.enabled', false);
+
+// Disable Firefox Hello.
+// user_pref('loop.enabled', false);
+
 // vim: tw=90 sts=-1 sw=3 et
