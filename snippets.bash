da54696b5d49187ec2c91ca835a936e1c74c1d98
Lukas Waymann <meribold@gmail.com>
2016-05-28 20:38:24 +0200

    Update snippets.bash

--- snippets.bash
+++ snippets.bash
@@ -11,6 +11,7 @@ git submodule foreach git clean -dfx # remove all untracked files of all submodu
 git submodule foreach git pull
 gpg-connect-agent reloadagent /bye # https://wiki.archlinux.org/index.php/GnuPG#Reload_the_agent
 i3-msg 'append_layout ~/.config/i3/scratchpad.json; move scratchpad'
+iw dev wlan0 link
 makepkg -sri
 mpc clear && mpc ls | mpc add
 pacman -Qeq --foreign > ~/packages-foreign.txt
@@ -18,6 +19,7 @@ pacman -Qeq --native > ~/packages-native.txt
 rsync -r -h --info=progress2 SRC DEST
 strfile ~/dotfiles/cookies
 sudo dhcpcd -B wlan0
+sudo iw dev wlan0 scan | less
 sudo mount /dev/sdb1 ~/sdb1
 sudo wpa_supplicant -i wlan0 -c ~/.wpa_supplicant.conf
 watch -n 1 cat /proc/acpi/ibm/{thermal,fan} /sys/class/power_supply/BAT1/energy_{now,full}
