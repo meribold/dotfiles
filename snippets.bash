03b45b347979361ff856d914b482919d10d2e1d8
Lukas Waymann <meribold@gmail.com>
2016-06-02 11:14:54 +0200

    Update snippets.bash

--- snippets.bash
+++ snippets.bash
@@ -1,7 +1,7 @@
 cat /proc/acpi/ibm/{thermal,fan} | head -n -3
 cat /sys/class/power_supply/BAT1/energy_{now,full}
 cower -u
-f=$(mktemp).png bash -c 'maim -s -b 2 -c .843,.373,.373 --nokeyboard "$f" || maim "$f" && imgurbash "$f"'
+f=$(mktemp).png bash -c 'maim -s -b 2 -c .843,.373,.373 --nokeyboard "$f" || maim "$f" && imgurbash "$f"; rm "$f"'
 feh --bg-center ~/images/1366x768/the-coming-darkness-noah-bradley.png
 fortune ~/dotfiles/cookies | cowsay -f ~/dotfiles/dynamic_duo.cow | lolcat
 git clean -dfx && git checkout -- .
@@ -21,6 +21,7 @@ strfile ~/dotfiles/cookies
 sudo dhcpcd -B wlan0
 sudo iw dev wlan0 scan | less
 sudo mount /dev/sdb1 ~/sdb1
+sudo umount ~/sdb1
 sudo wpa_supplicant -i wlan0 -c ~/.wpa_supplicant.conf
 watch -n 1 cat /proc/acpi/ibm/{thermal,fan} /sys/class/power_supply/BAT1/energy_{now,full}
 youtube-dl -o - 'XAAp_luluo0' | mplayer -cache 8192 -
