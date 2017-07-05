22278bd62623057d02fb509fe34a81c94fa7b516
Lukas Waymann <meribold@gmail.com>
2017-07-05 18:08:21 +0200

    i3: create two named screen sessions on startup
    Create two instead of one screen session in `startup.sh` and name them
    with screen's `-S` option.
    
    Naming them and explicitly specifying the screen session to attach to in
    the `wifail` script fixes it from failing if there is more than one
    session (`screen -x` quits with exit status 1 in that case).
    
    Creating two sessions is nice because attaching to the same session from
    xterms of different sizes (like I did) can by annoying: I usually don't
    want to view something started in a [scratchpad][1] terminal in a
    fullscreen one.
    
    [1]: https://i3wm.org/docs/userguide.html#_scratchpad

--- config/i3/startup.sh
+++ config/i3/startup.sh
@@ -4,12 +4,18 @@
 i3-msg 'append_layout ~/.config/i3/scratchpad.json; move scratchpad'
 i3-msg 'append_layout ~/.config/i3/workspace-2.json; move workspace 2'
 
-if ! screen -ls; then
-   # There's no screen session; create one.
-   screen -d -m
-   while ! screen -ls; do sleep .1; done
+# Create two screen sessions: one for the scratchpad and one for a fullscreen terminal.
+if ! screen -S scratchpad -ls; then
+   # There's no screen session named "scratchpad"; create one.
+   screen -S scratchpad -d -m
+   while ! screen -S scratchpad -ls; do sleep .1; done
    # Create some new windows inside the screen session.
-   screen -X screen && screen -X screen
+   screen -S scratchpad -X screen
+fi
+if ! screen -S fullscreen -ls; then
+   screen -d -m -S fullscreen
+   while ! screen -S fullscreen -ls; do sleep .1; done
+   screen -S fullscreen -X screen
 fi
 
 # Start a new xterm and attach to the screen session.  The xterm will be swallowed by a
@@ -18,7 +24,7 @@ fi
 #    session was started in detached mode and I'm starting a new xterm that immediately
 #    attaches.  Otherwise flow control is enabled.  I don't know why.
 # *  Preselect window 1 of the screen session with `-p 1`.
-xterm -e 'stty -ixon && exec screen -x -p 1' &
+xterm -e 'stty -ixon && exec screen -S scratchpad -x -p 1' &
 # Note: using `screen -r` for one xterm if we just created a detached session doesn't
 # always work when either xterm may attach first (`screen -r` won't attach to a session
 # that is already attached somewhere else).  Contrary to what the screen(1) man page says,
@@ -31,25 +37,25 @@ sleep .3
 # 24 otherwise).  Then, send ^L to the input buffer of window 1 (using screen's `stuff`
 # command) to clear the terminal screen.  All this does is make sure the prompt is at the
 # top and not somewhere in the middle.  See http://stackoverflow.com/a/25978564.
-screen -p 1 -X height -w 29
-screen -p 1 -X stuff '^L'
+screen -S scratchpad -p 1 -X height -w 29
+screen -S scratchpad -p 1 -X stuff '^L'
 
 # Select window 0 and repeat the process; it doesn't seem to work when the window isn't
 # selected.
-screen -X select 0
-screen -p 0 -X height -w 29
-screen -p 0 -X stuff '^L'
+screen -S scratchpad -X select 0
+screen -S scratchpad -p 0 -X height -w 29
+screen -S scratchpad -p 0 -X stuff '^L'
 
-# Start the second xterm and attach to the screen session.  Preselect window 2.  This
-# xterm gets placed on workspace 2 and I mainly use it for Vim.
-xterm -e 'stty -ixon && exec screen -x -p 2' &
+# Start the second xterm and attach to the screen session.  Preselect window 0.  This
+# xterm gets placed on workspace 2 and I mainly use it for Vim and Mutt.
+xterm -e 'stty -ixon && exec screen -S fullscreen -x -p 0' &
 
 # Wait until the xterm is started again (hopefully)...
 sleep .3
 
 # Apparently just sending ^L works fine now.  Maybe something is special about hidden
 # scratchpad workspaces.
-screen -p 2 -X stuff '^L'
+screen -S fullscreen -p 0 -X stuff '^L'
 
 # TODO: is it possible to set the size of the terminals emulated be screen without
 # attaching to the screen session first?
