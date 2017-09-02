f02267c767e684530ea85d060841a00ce396e679
Lukas Waymann <meribold@gmail.com>
2017-09-02 13:17:22 +0200

    Update snippets.bash

--- misc/snippets.bash
+++ misc/snippets.bash
@@ -7,7 +7,7 @@ cower -u
 f=$(mktemp).png bash -c 'maim -s -b 2 -c .843,.373,.373 --nokeyboard "$f" || maim "$f" && imgur.sh "$f"; rm "$f"'
 feh --bg-center ~/images/1366x768/the-coming-darkness-noah-bradley.png
 firefox-developer --new-tab $(gatewayip) # Try to open a router's web interface
-fortune meribold | cowsay -f dynamic-duo | lolcat
+fortune 50% meribold all | cowsay -W 72 -f dynamic-duo | lolcat
 git clean -dfx && git checkout -- .
 git commit -m 'Update commits recorded by submodules'
 git log --oneline --decorate --graph
@@ -25,6 +25,8 @@ iw dev wlan0 link
 journalctl -fu dhcpcd@wlan0 --no-tail -b -o cat
 journalctl -fu wpa_supplicant@wlan0 --no-tail -b -o cat
 jpm run -b /usr/bin/firefox-developer # test Firefox add-on
+killall -SIGUSR1 dunst # pause Dunst
+killall -SIGUSR2 dunst # resume Dunst
 latexmk -pdf -shell-escape
 mbsync gmail && notmuch new
 mount ~/sdb1 # mount known USB drive as normal user
@@ -46,11 +48,13 @@ sudo iw dev wlan0 scan | less
 sudo mount /dev/sdb1 ~/sdb1
 sudo pacman -Syu
 sudo systemctl poweroff
+sudo systemctl restart wpa_supplicant@wlan0
 sudo systemctl suspend
 sudo umount ~/sdb1
 sudo wpa_supplicant -i wlan0 -c ~/.wpa_supplicant.conf
 umount ~/sdb1
 watch -n 1 cat /proc/acpi/ibm/{thermal,fan} /sys/class/power_supply/BAT1/energy_{now,full}
+while :; do clear; fortune meribold | cowsay -W 72 -f dynamic-duo | lolcat; read -n 1; done
 xprop
 xrandr --output VGA-0 --auto --output HDMI-0 --auto # enable connected and disable disconnected external monitors
 xrandr --output VGA-0 --off --output HDMI-0 --off # disable external monitors
