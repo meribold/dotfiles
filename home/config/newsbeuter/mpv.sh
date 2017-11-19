fc5c715170ac5c1f44f59b00fa06394c2c0d4f85
Lukas Waymann <meribold@gmail.com>
2017-11-19 12:20:10 +0100

    Add mpv config file with format for youtube-dl
    https://github.com/mpv-player/mpv
    https://github.com/rg3/youtube-dl

--- home/config/newsbeuter/mpv.sh
+++ home/config/newsbeuter/mpv.sh
@@ -6,7 +6,5 @@
 screen -X split
 screen -X focus
 screen -X resize 10
-# TODO: it's probably possible and preferable to reduce the number of options passed to
-# mpv here, and instead configure it with a dedicated file.
-screen bash -c 'trap '\''screen -X focus top && screen -X only'\'' EXIT; mpv '\''--ytdl-format=best[height<=768]'\'' --keep-open -- '\'"$1"\'
+screen bash -c 'trap '\''screen -X focus top && screen -X only'\'' EXIT; mpv --keep-open -- '\'"$1"\'
 screen -X focus
