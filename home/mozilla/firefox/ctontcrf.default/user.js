e91c736fa718db7afa8296455371fad140b704b6
Lukas Waymann <meribold@gmail.com>
2018-04-26 21:56:36 +0200

    Firefox: stop disabling the Pocket extension

--- home/mozilla/firefox/ctontcrf.default/user.js
+++ home/mozilla/firefox/ctontcrf.default/user.js
@@ -1,9 +1,6 @@
 // Don't animate showing and hiding of the tab bar etc. when in fullscreen mode.
 user_pref('browser.fullscreen.animate', false);
 
-// Disable [Pocket](https://en.wikipedia.org/wiki/Pocket_%28application%29).
-user_pref('extensions.pocket.enabled', false);
-
 // Disable Firefox Hello.
 // user_pref('loop.enabled', false);
 
