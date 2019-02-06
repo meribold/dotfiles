3a0800112d5171430b80844ebd05eaf2bf8c323d
Lukas Waymann <meribold@gmail.com>
2019-02-06 14:26:37 +0800

    Newsboat: set title for `mpv` screen windows again
    I accidentally removed the `-t` option in 30633585a.  Also see 7178aadf.

--- home/config/newsboat/mpv.sh
+++ home/config/newsboat/mpv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-screen -S scratchpad -X screen bash -c 'mpv --keep-open -- '\'"$1"\'' || read' &&
+screen -S scratchpad -X screen -t "mpv $1" bash -c 'mpv --keep-open -- '\'"$1"\'' || read' &&
 # The previous command switched to the newly created window.  Undo that.
 screen -S scratchpad -X other
