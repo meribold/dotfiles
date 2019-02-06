e20b8acbbcd8dc784d25a057a24478a85672ab4c
Lukas Waymann <meribold@gmail.com>
2019-02-06 10:31:08 +0800

    Update `snippets.bash`
    I'll never get around to creating focused, clean commits for all these
    commands.  This is better than nothing.

--- misc/snippets.bash
+++ misc/snippets.bash
@@ -1,64 +1,162 @@
+# git clean -dfx && git checkout -- .
+# git submodule foreach git clean -dfx # remove all untracked files of all submodules
+# nx forget --force
+$BROWSER --new-tab $(gatewayip) # try to open a router's web interface
+>/dev/null trans -speak en: spinach
 PKGEXT=".pkg.tar" makepkg -sri # build and install an uncompressed package
 amixer set Master mute
-cat /sys/devices/virtual/thermal/thermal_zone{0,1}/temp /proc/acpi/ibm/fan | head -5 # temperatures
+bundle exec jekyll serve --drafts
 cat /sys/class/power_supply/BAT1/energy_{now,full}
+cat /sys/devices/virtual/thermal/thermal_zone{0,1}/temp /proc/acpi/ibm/fan | head -5 # temperatures
+cat /sys/module/usbcore/parameters/autosuspend
+checkupdates | grep "$(pacman -Qqe | awk '{ print "^"$1" " }')" | grep -v ' \(.\+\)-\(.\+\) -> \1-.\+$' | less -FX
 clear && neofetch --uptime_shorthand tiny --ascii && read
 cower -u
+curl ipinfo.io
+ds compton -o 1 -i 0.85 --no-fading-openclose --unredir-if-possible
+ds fcitx && sleep 0.5 && xmodmap ~/dotfiles/misc/xmodmaprc
+echo 1 | sudo tee /proc/sys/kernel/sysrq # enable all magic SysRq commands (e.g. REISUB)
+f() { mpc search any "$1" | mpc insert; }; f
 f=$(mktemp).png bash -c 'maim -s -b 2 -c .843,.373,.373 --nokeyboard "$f" || maim "$f" && imgur.sh "$f"; rm "$f"'
+f=~/screenshots/$(date "+%Y%m%dT%H%M%S").png bash -c 'maim -s -b 2 -c .843,.373,.373 --nokeyboard "$f" || maim "$f"'
 feh --bg-center ~/images/1366x768/the-coming-darkness-noah-bradley.png
-firefox-developer --new-tab $(gatewayip) # Try to open a router's web interface
+feh --sort mtime ~/.config/signal/attachments # view photos from Signal
+find / -name '*.desktop' 2>/dev/null | less
 fortune 50% meribold all | cowsay -W 72 -f dynamic-duo | lolcat
-git clean -dfx && git checkout -- .
+fusermount3 -u ~/zirakzigil
+g=$(mktemp) && f="$g".cpp && $VISUAL "$f" && g++ "$f" -o "$g" && "$g" # quickly hack some C++
+gds --color-words
+getent hosts zirakzigil snapscore.meribold.xyz meribold.xyz
+git check-attr --all
 git commit -m 'Update commits recorded by submodules'
+git fetch . WIP:master
 git pull --recurse-submodules && git submodule update
 git push --force-with-lease
-git submodule foreach git clean -dfx # remove all untracked files of all submodules
 git submodule foreach git pull
 git submodule update # doesn't change what commits are recorded in the superproject
 git submodule update --remote --merge # merge upstream submodule changes, updates recorded commits
+git-crypt status
+gpg --encrypt --armor --recipient D14CCBFF836E57327C252FDE7066AC79C4592C12
 gpg-connect-agent reloadagent /bye # https://wiki.archlinux.org/index.php/GnuPG#Reload_the_agent
-i3-msg 'append_layout ~/.config/i3/scratchpad.json; move scratchpad'
+i3-msg 'append_layout ~/.config/i3/scratchpad.json' && xterm -e 'stty -ixon && exec screen -S scratchpad -x -p 0' & sleep .3; i3-msg 'move scratchpad'
+i3-msg -- resize set 1370 381, move position -2 -2 # move and resize to scratchpad position and size
 ip address show dev wlan0
 ip route show dev wlan0
 iw dev wlan0 link
-journalctl -fu dhcpcd@wlan0 --no-tail -b -o cat
-journalctl -fu wpa_supplicant@wlan0 --no-tail -b -o cat
-jpm run -b /usr/bin/firefox-developer-edition # test Firefox add-on
+journalctl --no-tail -b -o cat -fu dhcpcd@eth0
+journalctl --no-tail -b -o cat -fu dhcpcd@wlan0
+journalctl --no-tail -b -o cat -fu wpa_supplicant@wlan0
+journalctl --user -n 100 -fu signal-cli.bash.service
 killall -SIGUSR1 dunst # pause Dunst
 killall -SIGUSR2 dunst # resume Dunst
+killall xbindkeys; (cd ~ && xbindkeys)
 latexmk -pdf -shell-escape
 mbsync gmail && notmuch new
+mount ~/usb-hdd
 mount ~/v8x # mount my USB drive (this only works because of an entry in my fstab(5))
 mount ~/v8x && { git pull usb; umount ~/v8x; } # pull from my USB drive
 mount ~/v8x && { git push usb; umount ~/v8x; } # push to my USB drive
 mount ~/v8x && { pass git pull; umount ~/v8x; } # pull to ~/.password-store
 mount ~/v8x && { pass git push; umount ~/v8x; } # push ~/.password-store
 mpc clear && mpc add / && mpc shuffle # make MPD queue all songs in random order
+mpc clear && mpc load favorites
 mpc toggleoutput 2 # toggle whether MPD produces output for cli-visualizer
-pacman -Qeq --foreign > ~/packages-foreign.txt
-pacman -Qeq --native > ~/packages-native.txt
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://3p8jLMz0lu8' # Taverns of Azeroth (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://7cy_RK04TUA' # Vanilla Winterspring (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://BV-v9bdMQp0' # Teldrassil (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://C0nAcUxPFqs' # Ironforge (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://Gl6g9CZTZL0' # Gothic (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://Gli5allDOa8' # Stranglethorn Vale (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://ID1KR37bqnQ' # Vanilla Darkshore (music & rain ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://JUoH8CzIkDo' # Avatar mix 1
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://MW4fASDkQXA' # Elwynn (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://PGOjUZpYu-U' # Vanilla Feralas (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://QHV_FTIm6zY' # Vanilla Ironforge (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://RhYuUV4wSC8' # Vanilla Blackfathom Deeps (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://UrJTZZibdHk' # LOTR (calm music mix)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://WwcGGkvEIHE' # The Night Elves of Vanilla (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://XDKPs_CMd0E' # Vanilla Moonglade (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://YDOp-IlsO6I' # Winterspring (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://YONdeDeSgrQ' # Barrens (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://YTnLxsYOj8w' # Vanilla Tanaris (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://Y__NCf9lYPk' # Shire, LOTRO (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://_SBQvd6vY9s' # LOTR soundtrack
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://b-gMnQIV5tU' # Avatar mix 2
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://f-Ic9feSwcg' # Zangarmarsh (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://ix2CRYd2FT8' # The Tauren of Vanilla (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://j7GK9KPvabk' # Ashenvale (ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://ku4Z8GrHjIo' # Isle of Thunder (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://q8PqlJBeuT0' # Mulgore (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://rbM0eGQepgc' # Moonglade (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://u3qIZG11mSg' # Stormwind (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://wLY-5S-6xuY' # Darnassus (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio 'ytdl://xTPn_Nk_KrM' # Ashenvale (music & ambience)
+mpv --input-file=/tmp/mpvfifo --ytdl-format bestaudio --shuffle 'ytdl://PLBCLqOzi7nCR7cfDldMpVICZ5BSdmQCT0'
+mpv av://v4l2:/dev/video0
+mutt_pid=$(pgrep neomutt) && sudo strace -p "$mutt_pid"
+neomutt -s 'Hi.' 'meribold@gmail.com' <<< ''
+nohup xdg-open file &>/dev/null <&1 &
+nvim -q <(grep -n .e.e. /tmp/tmp.*) # TODO: Bash function?
+nx copy --fast --to rsync.net
+nx info
+nx init 'Toshiba USB HDD'
+nx sync --cleanup
+nx sync --no-resolvemerge --no-commit
+paccache -rk1 # remove all but the most recent cached versions of ALL packages
+paccache -ruk0 # remove ALL cached versions of uninstalled packages
+pacman -Qe | grep -v "$(pacman -Qqeg base-devel base)" # print explicitly installed packages not in base or base-devel
+pacman -Qeq --foreign > ~/dotfiles/misc/foreign-packages.txt
+pacman -Qeq --native > ~/dotfiles/misc/native-packages.txt
+pacman -Qii | awk '/^MODIFIED/ {print $2}' # list changed backup files
 pacman -Qtdq # list (real) orphan packages
 pip list --local --outdated # list outdated Python packages; use `pip install --user -U` to upgrade them
 pip list --user --outdated # list outdated Python packages; use `pip install -U` to upgrade them
+python -c 'import cv2; print(cv2.getBuildInformation())' | less
 reflector --age 1 --latest 200 --sort rate -n 10 | sudo tee /etc/pacman.d/mirrorlist # generate new mirror list for pacman
+rofi -combi-modi window,drun -modi combi -show
 rofi -modi drun,run -matching fuzzy -show
-rsync -r -h --info=progress2 SRC DEST
+route -n # get the IP address of the default gateway (router)
+rsync -rh --info=progress2 -e 'ssh -i '~'/.ssh/zirakzigil.pem' meribold@zirakzigil:/home/meribold/snapscore-server/received_images/ ~/snapscore-images
+rsync -rh --info=progress2 SRC DEST
+sco() { git checkout --detach && git reset "$1" && git checkout "$1"; }; sco
+scp -i ~/.ssh/zirakzigil.pem meribold@zirakzigil:src dest
+sensors
+sleep 1 && i3-msg border pixel 1
+slop -b 2 -c .843,.373,.373 -t 9999 --nokeyboard >/dev/null && i3-msg border none # remove any border from a container
+slop -b 2 -c .843,.373,.373 -t 9999 --nokeyboard >/dev/null && i3-msg border pixel 1 # add a border to a container
+ssh -ti ~/.ssh/zirakzigil.pem meribold@zirakzigil screen -x
+sshfs meribold@zirakzigil:/home/meribold/ ~/zirakzigil -o IdentityFile=~/.ssh/zirakzigil.pem
 sudo dhcpcd -B wlan0
+sudo etckeeper commit
 sudo ip link set wlan0 up
+sudo iw dev wlan0 connect SSID # join the open WiFi network with the given SSID
 sudo iw dev wlan0 scan | less
-mount ~/usb-hdd
+sudo pacman -Rns $(pacman -Qtdq) # recursively remove (real) orphan packages
 sudo pacman -Syu
 sudo systemctl poweroff
+sudo systemctl restart dhcpcd@wlan0
 sudo systemctl restart wpa_supplicant@wlan0
+sudo systemctl start dhcpcd@eth0 && sudo systemctl stop wpa_supplicant@wlan0
 sudo systemctl suspend
-umount ~/usb-hdd
 sudo wpa_supplicant -i wlan0 -c ~/.wpa_supplicant.conf
+systemctl --user restart signal-cli.bash.service
+systemctl --user start signal-cli.bash.service
+systemctl --user stop signal-cli.bash.service
+telnet mapscii.me
+trans :zh-TW -b - | s
+umount ~/usb-hdd
 umount ~/v8x
+vim -u NONE
 watch -n 1 cat /proc/acpi/ibm/{thermal,fan} /sys/class/power_supply/BAT1/energy_{now,full}
+wgetpaste
 while :; do clear; fortune meribold | cowsay -W 72 -f dynamic-duo | lolcat; read -n 1; done
+xdg-open file &>/dev/null <&1 & disown
+xev # interactively enter keys and get keysyms
 xprop
+xrandr --output HDMI-0 --auto --right-of LVDS # FIXME: untested
 xrandr --output VGA-0 --auto --output HDMI-0 --auto # enable connected and disable disconnected external monitors
 xrandr --output VGA-0 --off --output HDMI-0 --off # disable external monitors
+xsel --clipboard | wgetpaste --tee -C
 xwininfo
-youtube-dl -o - 'XAAp_luluo0' | mplayer -cache 8192 -
+youtube-dl -x --audio-format mp3 --audio-quality 0 'GmtTDvNcXcU'
 { checkupdates & cower -u; } | less -FX # check for updates to native and foreign (AUR) packages
