266d8ff7508889c8604277435cb9400b7e37b6b9
Lukas Waymann <git@meribold.xyz>
2019-07-09 22:58:01 +0800

    Put Newsboat into a dedicated Screen session
    The mpv keybind uses this screen session now.

--- home/config/newsboat/mpv.sh
+++ home/config/newsboat/mpv.sh
@@ -2,6 +2,6 @@
 
 # TODO: consider <https://i3wm.org/docs/userguide.html#no_focus>.
 
-screen -S scratchpad -X screen -t "mpv $1" bash -c 'mpv --keep-open -- '\'"$1"\'' || read' &&
+screen -X screen -t "mpv $1" bash -c 'mpv --keep-open -- '\'"$1"\'' || read' &&
 # The previous command switched to the newly created window.  Undo that.
-screen -S scratchpad -X other
+screen -X other
