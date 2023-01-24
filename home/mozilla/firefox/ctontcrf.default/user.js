718e6598e638591d1e533f622f3c40465bb7bfd8
Lukas Waymann <git@meribold.org>
2023-01-24 23:16:06 +0800

    Firefox: remove commented-out line in user.js

--- home/mozilla/firefox/ctontcrf.default/user.js
+++ home/mozilla/firefox/ctontcrf.default/user.js
@@ -5,6 +5,3 @@ user_pref('browser.fullscreen.animate', false);
 user_pref('intl.date_time.pattern_override.date_short', 'yyyy-MM-dd');
 user_pref('intl.date_time.pattern_override.time_short', 'HH:mm');
 user_pref('intl.date_time.pattern_override.connector_short', '{1} {0}');
-
-// Disable Firefox Hello.
-// user_pref('loop.enabled', false);
