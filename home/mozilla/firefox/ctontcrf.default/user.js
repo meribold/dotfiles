0169db6674bca803308aba5a2bb53d89eb41a218
Lukas Waymann <git@meribold.org>
2023-01-24 20:59:21 +0800

    Format dates/times in the FF Library window nicely
    Format dates and times in Firefox's Library window (Control+Shift+O)
    nicely.
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1426907
    https://redd.it/qy8dkx

--- home/mozilla/firefox/ctontcrf.default/user.js
+++ home/mozilla/firefox/ctontcrf.default/user.js
@@ -1,5 +1,10 @@
 // Don't animate showing and hiding of the tab bar etc. when in fullscreen mode.
 user_pref('browser.fullscreen.animate', false);
 
+// Format dates and times in the Library window (Control+Shift+O) nicely.
+user_pref('intl.date_time.pattern_override.date_short', 'yyyy-MM-dd');
+user_pref('intl.date_time.pattern_override.time_short', 'HH:mm');
+user_pref('intl.date_time.pattern_override.connector_short', '{1} {0}');
+
 // Disable Firefox Hello.
 // user_pref('loop.enabled', false);
