cee7689332a061aa10dca510cf2c6107838a5338
Lukas Waymann <git@meribold.org>
2023-01-25 16:18:10 +0800

    Reformat user.js using Prettier
    https://prettier.io
    https://github.com/prettier/prettier

--- home/mozilla/firefox/ctontcrf.default/user.js
+++ home/mozilla/firefox/ctontcrf.default/user.js
@@ -1,7 +1,7 @@
 // Don't animate showing and hiding of the tab bar etc. when in fullscreen mode.
-user_pref('browser.fullscreen.animate', false);
+user_pref("browser.fullscreen.animate", false);
 
 // Format dates and times in the Library window (Control+Shift+O) nicely.
-user_pref('intl.date_time.pattern_override.date_short', 'yyyy-MM-dd');
-user_pref('intl.date_time.pattern_override.time_short', 'HH:mm');
-user_pref('intl.date_time.pattern_override.connector_short', '{1} {0}');
+user_pref("intl.date_time.pattern_override.date_short", "yyyy-MM-dd");
+user_pref("intl.date_time.pattern_override.time_short", "HH:mm");
+user_pref("intl.date_time.pattern_override.connector_short", "{1} {0}");
