2dc8e79c35c7fa441975e7730c303b8c469c9657
Lukas Waymann <git@meribold.org>
2025-03-24 22:58:43 +0100

    Small tweaks

--- misc/oneliners.smial.bash
+++ misc/oneliners.smial.bash
@@ -1,7 +1,6 @@
 ds compton -o 1 -i 0.85 --no-fading-openclose --unredir-if-possible
 fortune 50% meribold all | cowsay -W 72 -f dynamic-duo | lolcat
 git push esgaroth snapshots-meribold-smial
-i3-msg 'append_layout ~/.config/i3/scratchpad.json' && xterm -e 'stty -ixon && exec screen -S scratchpad -x -p 0' & sleep .3; i3-msg 'move scratchpad'
 nx describe here Smial
 nx init Smial
 date; sudo backup; date; systemctl suspend
